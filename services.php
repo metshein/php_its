87cea7e559ec252f7b3acbc46bcdd68fbacdbcda
metshein <metshein@gmail.com>
2024-03-07 13:38:02 +0200

    sdd

--- services.php
+++ services.php
@@ -1,4 +1,6 @@
 <h1>Teenused</h1>
+
+<div class="row row-cols-1 row-cols-md-4 g-4">
 <?php
     //faili avamine
     $products = "products.csv";
@@ -8,11 +10,22 @@
     while (!feof($minu_csv)) {
         //ühe rea saamine, eraldatud komaga
         $rida = fgetcsv($minu_csv, filesize($products), ",");
-        print_r($rida);
+        //print_r($rida);
+        // echo "$rida[1] - $rida[3]€<br>";
+        echo '
+        <div class="col">
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">'.$rida[1].'</h5>
+                <p class="card-text">'.$rida[2].'</p>
+                <p class="card-text">'.$rida[3].'</p>
+                </div>
+            </div>
+        </div>
+        ';
     }
     
-
-    
-
-
 ?>
+
+</div>
