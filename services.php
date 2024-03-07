fccf25958e50da610aad8944025acfdc6af8661f
metshein <metshein@gmail.com>
2024-03-07 13:06:50 +0200

    sss

--- services.php
+++ services.php
@@ -1,1 +1,18 @@
 <h1>Teenused</h1>
+<?php
+    //faili avamine
+    $products = "products.csv";
+    $minu_csv = fopen($products, "r");
+
+    //kõikide ridade saamine feof = file-end-of-file
+    while (!feof($minu_csv)) {
+        //ühe rea saamine, eraldatud komaga
+        $rida = fgetcsv($minu_csv, filesize($products), ",");
+        print_r($rida);
+    }
+    
+
+    
+
+
+?>
