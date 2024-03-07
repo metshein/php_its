60eaa2e5098671978242fe1525f65ddf16904b9b
metshein <metshein@gmail.com>
2024-03-07 14:12:49 +0200

    dd

--- services.php
+++ services.php
@@ -1,6 +1,38 @@
 <h1>Teenused</h1>
+<form action="
+" method="get">
+    <label for="nimetus">Toote nimetus</label>
+    <input type="text" name="nimetus" required><br>
 
-<div class="row row-cols-1 row-cols-md-4 g-4">
+    <label for="kirjeldus">Toote kirjeldus</label>
+    <input type="text" name="kirjeldus" required><br>
+
+    <label for="hind">Toote hind</label>
+    <input type="number" min="0.00" max="100.00" step="0.01" name="hind" required><br>
+
+    <input type="hidden" name="page" value="services">
+
+    <input class="btn btn-success" type="submit" value="Lisa uus toode">
+</form>
+<?php
+if (isset($_GET['nimetus'])) {
+    $read=[];
+
+    $nimetus = array_push($read, $_GET['nimetus']);
+    $kirjeldus = array_push($read, $_GET['kirjeldus']);
+    $hind = array_push($read, $_GET['hind']);
+    
+    $path = 'products.csv';
+    $fp = fopen($path, 'a'); 
+    fputcsv($fp, $read);
+    //print_r($nimetus);
+}
+
+?>
+
+
+
+<div class="row row-cols-1 row-cols-md-4 g-4 pt-5">
 <?php
     //faili avamine
     $products = "products.csv";
@@ -15,11 +47,11 @@
         echo '
         <div class="col">
             <div class="card">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="https://picsum.photos/400/400" class="card-img-top" alt="'.$rida[1].'">
                 <div class="card-body">
                 <h5 class="card-title">'.$rida[1].'</h5>
                 <p class="card-text">'.$rida[2].'</p>
-                <p class="card-text">'.$rida[3].'</p>
+                <p class="card-text">'.$rida[3].'€</p>
                 </div>
             </div>
         </div>
