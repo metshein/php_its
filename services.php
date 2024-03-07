a4a26251f6ae9dcfe82017903780047ab263c169
metshein <metshein@gmail.com>
2024-03-07 15:14:09 +0200

    ddd

--- services.php
+++ services.php
@@ -1,6 +1,5 @@
 <h1>Teenused</h1>
-<form action="
-" method="get">
+<form action="" method="post" enctype="multipart/form-data">
     <label for="nimetus">Toote nimetus</label>
     <input type="text" name="nimetus" required><br>
 
@@ -10,25 +9,34 @@
     <label for="hind">Toote hind</label>
     <input type="number" min="0.00" max="100.00" step="0.01" name="hind" required><br>
 
+    <label for="lisapilt"></label>
+    <input type="file" name="lisapilt"><br>
+
     <input type="hidden" name="page" value="services">
 
     <input class="btn btn-success" type="submit" value="Lisa uus toode">
 </form>
 <?php
-if (isset($_GET['nimetus'])) {
+if (isset($_POST['nimetus'])) {
+    $ajutine_fail =  $_FILES['lisapilt']['tmp_name'];
+    move_uploaded_file($ajutine_fail, 'img/'.$_FILES['lisapilt']['name']);
     $read=array();
 
     $id = array_push($read,count(file('products.csv'))+1);
 
-    $nimetus = array_push($read, $_GET['nimetus']);
-    $kirjeldus = array_push($read, $_GET['kirjeldus']);
-    $hind = array_push($read, $_GET['hind']);
-    
+    $nimetus = array_push($read, $_POST['nimetus']);
+    $kirjeldus = array_push($read, $_POST['kirjeldus']);
+    $hind = array_push($read, $_POST['hind']);
+    $pildinimi = array_push($read, $_FILES['lisapilt']['name']);
+
+
     $path = 'products.csv';
     $fp = fopen($path, 'a'); 
     fputcsv($fp, $read);
     //print_r($nimetus);
     fclose($fp);
+    //suunab "puhtale" lehele
+    header('Location:03.php?page=services');
 }
 
 ?>
@@ -51,7 +59,7 @@ if (isset($_GET['nimetus'])) {
             echo '
             <div class="col">
                 <div class="card">
-                    <img src="https://picsum.photos/400/400" class="card-img-top" alt="'.$rida[1].'">
+                    <img src="img/'.$rida[4].'" class="card-img-top" alt="'.$rida[1].'">
                     <div class="card-body">
                     <h5 class="card-title">'.$rida[1].'</h5>
                     <p class="card-text">'.$rida[2].'</p>
