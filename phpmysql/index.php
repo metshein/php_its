9832d51d8bc396efe17c7a552ad7d0c2828a3a31
metshein <metshein@gmail.com>
2024-03-21 16:11:35 +0200

    sdf

--- phpmysql/index.php
+++ phpmysql/index.php
@@ -9,12 +9,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
+    
     <div class="container">
     <h1>Maailma imelikumad palad!</h1>
-<div class="row row-cols-1 row-cols-md-6 g-4">
+    <form action="" method="get">
+        Otsi: <input type="text" name="s">
+        <input type="submit" value="Otsi">
+    </form>
+<div class="row row-cols-1 row-cols-md-6 g-4 pt-4">
       <?php
-    //päring mille saadan andmebaasi
-        $paring = "SELECT * FROM albumid";
+//otsing
+    if (!empty($_GET["s"])) {
+        $s = $_GET["s"];
+        $paring = 'SELECT album, hind FROM albumid WHERE album LIKE "%'.$s.'%" ';
+    } else {
+        //päring mille saadan andmebaasi
+        $paring = " SELECT album, hind FROM albumid ORDER BY artist ASC LIMIT 10";
+    }
+
+
+    
     //saadan soovitud ühendusele minu päringu
         $valjund = mysqli_query($yhendus, $paring);
     //sikutame andmebaasist kõik vastuse
@@ -24,7 +38,7 @@
             echo '
             <div class="col">
                 <div class="card">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="https://picsum.photos/400/400" alt="pildike">
                 <div class="card-body">
                     <h5 class="card-title">'.$rida['album'].'</h5>
                     <p class="card-text">'.$rida['hind'].'€</p>
@@ -33,8 +47,7 @@
                 </div>
             </div>
             ';
-        }    
-
+        }
     ?>
 </div>
    </div>
